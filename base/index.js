b604675f10c83d1053c7c1e19aaf2fb5b2d6a4d1
Leicher Julien <sky@localhost.localdomain>
2016-10-09 22:10:06 +0200

    Some more work

--- base/index.js
+++ base/index.js
@@ -3,10 +3,12 @@ import * as actions from './actions';
 import * as actionTypes from './actionTypes';
 import * as colors from './colors';
 import * as storage from './storage';
+import components from './components';
 
 export default {
   actions,
   actionTypes,
+  ...components,
   constants,
   colors,
   storage,
--- onboarding/components/Loading.js
+++ onboarding/components/Loading.js
@@ -2,9 +2,16 @@ import React from 'react';
 import {
   View,
 } from 'react-native';
+import base from './../../base';
+
+const { Navbar } = base;
 
 const Loading = () => (
   <View />
+);
+
+Loading.renderNavigationBar = () => (
+  <Navbar />
 );
 
 export default Loading;
