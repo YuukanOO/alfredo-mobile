5987b463acf1d52bb666cb25cb137bcd905002d8
Leicher Julien <jleicher@gmail.com>
2016-10-19 21:51:05 +0200

    Updated selectors getServerInfo, must update all calls

--- src/house/reducer.js
+++ src/house/reducer.js
@@ -8,6 +8,7 @@ const initialState = {
     version: null,
     token: null,
   },
+  editing: false,
   currentRoom: null,
   rooms: {},
   devices: {},
--- src/house/selectors.js
+++ src/house/selectors.js
@@ -1,20 +1,27 @@
 import { createSelector } from 'reselect';
 import { NAME } from './constants';
 
-export const getServerInfo = state => ({
-  ...state[NAME].server,
-  body: data => ({
-    headers: {
-      Authorization: (state[NAME].server && state[NAME].server.token) ? `Bearer ${state[NAME].server.token}` : null,
-    },
-    body: data,
-  }),
-});
 export const getRawServerInfo = state => (state[NAME].server || {});
+export const getServerInfo = createSelector(
+  getRawServerInfo,
+  serverInfo => ({
+    ...serverInfo,
+    request: (path, body) => [
+      `${serverInfo.local}/${path}`,
+      {
+        headers: {
+          Authorization: (serverInfo && serverInfo.token) ? `Bearer ${serverInfo.token}` : null,
+        },
+        body,
+      },
+    ],
+  })
+);
 export const getRooms = state => state[NAME].rooms;
 export const getRoomsArray = createSelector(getRooms, rooms => Object.values(rooms));
 export const getDevices = state => state[NAME].devices;
 export const getAdapters = state => state[NAME].adapters;
+export const getEditing = state => state[NAME].editing;
 export const getCurrentRoom = createSelector(
     getRooms,
     state => state[NAME].currentRoom,
